cb554bbb8058772a682115d5dd4b97ab3cbe74d6
Zishan21830 <103773696+Zishan21830@users.noreply.github.com>
2025-01-09 15:07:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>Farhan</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "dark-black": "#0d0e12",
+      "white" : '#ffffff',
+    },
+    screens: {
+      xs: "0px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily: {
+      lora: ["Lora", "serif"],
+      playwrite: ['"Playwrite AU SA"', "cursive"],
+      poppins: ["Poppins", "sans-serif"],
+      mono: ['"Courier New"', "Courier", "monospace"],
+    },
+  },
+  plugins: [],
+};
